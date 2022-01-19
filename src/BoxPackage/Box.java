e2b2d655389559c4536c424a912c6d5f0817e84e
mallona <alena.parkhatskaya@gmail.com>
2022-01-19 09:41:19 +0200

    Initial commit

---
+++ src/BoxPackage/Box.java
@@ -0,0 +1,46 @@
+package BoxPackage;
+
+public class Box {
+    private double width;
+    private double height;
+    private double length;
+
+    public Box(double width, double height, double length) {
+        this.width = width;
+        this.height = height;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double volume() {
+        double vol = width * height * length;
+        return vol;
+    }
+
+    {
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import BoxPackage.Box;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Box box1 = new Box(5,2,3);
+        Box box2 = new Box(1.2,3,4.5);
+        Box box3 = new Box(1,2,3);
+        Box box4 = new Box(3.4,7.9,2.2);
+        Box box5 = new Box(6,2,8);
+
+        System.out.println("Volumes of Box1 is: " + "\nVolumes of Box2 is: " + "\nVolumes of Box3 is: " + "\nVolumes of Box4 is: " + "\nVolumes of Box5 is: ");
+    }
+}
